d537e9db8d99c9c3dfeeab8c8006945e47bb08d2
kelkeke <106389648+kelkeke@users.noreply.github.com>
2023-07-24 16:29:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/{}'.format(os.environ.get['SECRET']),  methods=['POST'])
+@app.route('/{}'.format(os.environ.get('SECRET')),  methods=['POST'])
 def index():
     print("data: " + str(request.get_json()))
     
-    requests.post(os.environ.get['URL'], json=request.get_json())
+    requests.post(os.environ.get('URL'), json=request.get_json())
     
     return "ok"
