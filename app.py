9ba925fe9da7b3c82c9ab826941f09a7beb5dfdc
kelkeke <106389648+kelkeke@users.noreply.github.com>
2023-07-24 16:10:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/',  methods=['POST'])
+@app.route('/{}'.format(secret),  methods=['POST'])
 def index():
     print("data: " + str(request.get_json()))
-    print("headers: " + str(request.headers))
     
     return "ok"
